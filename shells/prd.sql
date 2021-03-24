ee410ba0ff47fb028452024283b1f29ec6a41fa1
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-03-24 01:35:11 -0300

    prd.sql

--- shells/prd.sql
+++ shells/prd.sql
@@ -7,18 +7,22 @@ SELECT cast(LPAD(TRIM(P.no), 6, '0') AS CHAR) AS codigo,
        P.typeno,
        T.name                                 AS typeName,
        CAST(LPAD(P.clno, 6, '0') AS CHAR)     AS clno,
-       cl.name                                AS clname
+       cl.name                                AS clname,
+       0 as marca, 0 as categoria, '' AS descricaoCompleta, 0 as bitola, '' as imagem, 0 as peso, 0 as altura, 0 as comprimento, 
+  0 as largura, 0 as editado, '' as textLink, '' as especificacoes, P.sp/100 as precoCheio, S.ncm, '' AS gradeCompleta, '' as cor, current_timestamp as dataHoraMudanca 
 FROM sqldados.prd           AS P
   LEFT JOIN sqldados.grade  AS G
-	      ON G.no = MID(P.grade_l, 1, 10) * 1
+              ON G.no = MID(P.grade_l, 1, 10) * 1
   LEFT JOIN sqldados.grdval AS V
-	      ON V.gradeno = G.no AND (V.bits & POW(2, 0)) = 0
+              ON V.gradeno = G.no AND (V.bits & POW(2, 0)) = 0
   LEFT JOIN sqldados.prdbar AS B
-	      ON P.no = B.prdno AND V.val = B.grade
+              ON P.no = B.prdno AND V.val = B.grade
   LEFT JOIN sqldados.vend   AS F
-	      ON F.no = P.mfno
+              ON F.no = P.mfno
   LEFT JOIN sqldados.type   AS T
-	      ON T.no = P.typeno
+              ON T.no = P.typeno
   LEFT JOIN sqldados.cl
-	      ON cl.no = P.clno
+              ON cl.no = P.clno
+  LEFT JOIN sqldados.spedprd AS S
+    ON P.no = S.prdno
 GROUP BY codigo, grade;
