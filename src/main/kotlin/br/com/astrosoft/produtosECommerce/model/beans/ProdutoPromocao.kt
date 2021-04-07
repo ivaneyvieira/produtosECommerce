e582f85b96845149109d82bd775ba692eb360a99
Ivaney Vieira <ivaneyvieira@gmail.com>
2021-04-07 08:58:36 -0300

    Bug na hora de selecionar os produtos promocionais

--- src/main/kotlin/br/com/astrosoft/produtosECommerce/model/beans/ProdutoPromocao.kt
+++ src/main/kotlin/br/com/astrosoft/produtosECommerce/model/beans/ProdutoPromocao.kt
@@ -48,6 +48,21 @@ class ProdutoPromocao(
       }
     }
   }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as ProdutoPromocao
+
+    if (codigo != other.codigo) return false
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    return codigo.hashCode()
+  }
 }
 
 data class FiltroProdutosPromocional(
