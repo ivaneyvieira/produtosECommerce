2abb74c8bb12a5533a8b3acb7a95a54250fce87d
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-04-20 10:21:42 -0300

    Busca de produtos pelo o código de barras

---
+++ src/main/kotlin/br/com/astrosoft/produtosECommerce/model/beans/ProdutoCodigo.kt
@@ -0,0 +1,3 @@
+package br.com.astrosoft.produtosECommerce.model.beans
+
+class ProdutoCodigo(val codigo: String, val grade: String, val barcode: String)
--- src/main/kotlin/br/com/astrosoft/produtosECommerce/model/services/ServiceQueryProdutoConferencia.kt
+++ src/main/kotlin/br/com/astrosoft/produtosECommerce/model/services/ServiceQueryProdutoConferencia.kt
@@ -3,10 +3,17 @@ package br.com.astrosoft.produtosECommerce.model.services
 import br.com.astrosoft.framework.model.IServiceQuery
 import br.com.astrosoft.framework.model.SortOrder
 import br.com.astrosoft.produtosECommerce.model.beans.FiltroProdutoConferencia
+import br.com.astrosoft.produtosECommerce.model.beans.ProdutoBarcode
+import br.com.astrosoft.produtosECommerce.model.beans.ProdutoCodigo
 import br.com.astrosoft.produtosECommerce.model.beans.ProdutoConferencia
 import br.com.astrosoft.produtosECommerce.model.local
 import br.com.astrosoft.produtosECommerce.model.saci
 import br.com.astrosoft.produtosECommerce.model.xlsx.PrecosEcomerce
+
+private fun ProdutoBarcode?.toProdutoCodigo(): ProdutoCodigo? {
+  val produto = this ?: return null
+  return ProdutoCodigo(codigo = produto.codigo.toString(), grade = produto.grade, barcode = produto.barcode)
+}
 
 class ServiceQueryProdutoConferencia : IServiceQuery<ProdutoConferencia, FiltroProdutoConferencia> {
   override fun count(filter: FiltroProdutoConferencia): Int {
@@ -25,11 +32,13 @@ class ServiceQueryProdutoConferencia : IServiceQuery<ProdutoConferencia, FiltroP
     local.apagaPrecos()
     val precosSaci = saci.precoSaci().groupBy { it.codigo }.mapValues { it.value.firstOrNull() }
     val produtosLocal = local.produtosBarcode()
-    val produtosBarcode = produtosLocal.groupBy { it.barcode }.mapValues { it.value.firstOrNull() }
-    val produtosCodigo = produtosLocal.groupBy { it.codigo }.mapValues { it.value.firstOrNull() }
+    val produtoBarcodeSaci = saci.produtoBarcodeSaci().groupBy { it.barcode }.mapValues { it.value.firstOrNull() }
+    val produtosBarcode = produtosLocal.groupBy { it.barcode }.mapValues { it.value.firstOrNull()?.toProdutoCodigo() }
+    val produtosCodigo = produtosLocal.groupBy { it.codigo }.mapValues { it.value.firstOrNull()?.toProdutoCodigo() }
     precos.forEach {
-      val produto = produtosBarcode[it.codigo] ?: produtosCodigo[it.codigo.toIntOrNull()]
-      val prdSaci = precosSaci[produto?.codigo]
+      val produto =
+              produtoBarcodeSaci[it.codigo] ?: produtosBarcode[it.codigo] ?: produtosCodigo[it.codigo.toIntOrNull()]
+      val prdSaci = precosSaci[produto?.codigo?.toIntOrNull()] ?: precosSaci[it.codigo.toIntOrNull()]
 
       local.addPrecoConferencia(
         refid = it.codigo,
