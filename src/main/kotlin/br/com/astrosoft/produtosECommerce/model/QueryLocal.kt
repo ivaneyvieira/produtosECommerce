9479494f5930de4a9eb57efa2a1c51558ac019cc
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-05-13 15:33:46 -0300

    Filtro para escolher o tipo de filtro

--- src/main/kotlin/br/com/astrosoft/produtosECommerce/model/QueryLocal.kt
+++ src/main/kotlin/br/com/astrosoft/produtosECommerce/model/QueryLocal.kt
@@ -371,6 +371,7 @@ GROUP BY barcode"""
       addOptionalParameter("marca", filtro.marca)
       addOptionalParameter("categoria", filtro.categoria)
       addOptionalParameter("promocao", if (filtro.promocao) "S" else "N")
+      addOptionalParameter("diferencai", filtro.diferenca.id)
     }
   }
 
