4818de727e113e991c42ca8f2a9bf63bde89b63d
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-07-29 12:37:17 -0300

    Ajuste nas colunas que devem ser redimencionadas

--- src/main/kotlin/br/com/astrosoft/produtosECommerce/view/vtex/PainelGridPromocao.kt
+++ src/main/kotlin/br/com/astrosoft/produtosECommerce/view/vtex/PainelGridPromocao.kt
@@ -1,8 +1,11 @@
 package br.com.astrosoft.produtosECommerce.view.vtex
 
+import br.com.astrosoft.framework.util.format
 import br.com.astrosoft.framework.view.*
 import br.com.astrosoft.produtosECommerce.model.beans.FiltroVtex
 import br.com.astrosoft.produtosECommerce.model.beans.Vtex
+import br.com.astrosoft.produtosECommerce.model.planilha.CampoNumber
+import br.com.astrosoft.produtosECommerce.model.planilha.CampoString
 import br.com.astrosoft.produtosECommerce.model.planilha.PlanilhaVtexPreco
 import br.com.astrosoft.produtosECommerce.model.services.ServiceQueryVtex
 import br.com.astrosoft.produtosECommerce.model.xlsx.EColunaNaoEncontrada
@@ -219,7 +222,23 @@ class PainelGridPromocao(val view: IVtexView, val serviceQueryVtex: ServiceQuery
 
   private fun HasComponents.downloadExcel() {
     val button = LazyDownloadButton(VaadinIcon.TABLE.create(), { filename() }, {
-      val planilha = PlanilhaVtexPreco()
+      val planilha = PlanilhaVtexPreco {
+        listOf(
+          CampoString("Sku ID") { skuId.toString() },
+          CampoString("Id Prod") { idProd.toString() },
+          CampoString("Nome SKU") { nomeSku },
+          CampoString("Referencia SKU") { referenciaSKU },
+          CampoString("Cód Saci") { codigo },
+          CampoNumber("Preço Compor") { precoCompor ?: 0.00 },
+          CampoString("Nº Prom") { promono.toString() },
+          CampoString("Validade") { validade.format() },
+          CampoNumber("Preço Promo") { promoprice ?: 0.00 },
+          CampoNumber("Preço Ref") { refprice ?: 0.00 },
+          CampoString("Validade Vtex") { validadeVtex.format() },
+          CampoNumber("Promo Vtex") { promoVtex ?: 0.00 },
+          CampoNumber("Preço Vtex") { preco },
+              )
+      }
       val bytes = planilha.grava(allItens())
       ByteArrayInputStream(bytes)
     })
