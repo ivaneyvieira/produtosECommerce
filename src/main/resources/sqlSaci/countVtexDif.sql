fcbf8435444e57f9aeeb5bd9d1768349f16af799
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-08-05 12:57:37 -0300

    Adição da coluna com peço procional do Editor

--- src/main/resources/sqlSaci/countVtexDif.sql
+++ src/main/resources/sqlSaci/countVtexDif.sql
@@ -25,8 +25,9 @@ WHERE (skuId LIKE CONCAT(@SKU, '%') OR @SKU = '')
        (IFNULL(precoList, 0) != IFNULL(refprice, 0) AND :diferenca = 'LIST') OR
        (IFNULL(precoCompor, 0) != 0 AND :diferenca = 'COMPOR') OR
        (IFNULL(promoVtex, 0) != 0 AND :diferenca = 'PRICEBASE') OR
-       (IFNULL(preco, 0) != IFNULL(promoprice, 0) AND IFNULL(refprice, 0) != IFNULL(preco, 0) AND
-	:diferenca = 'BASE') OR
+       ((IFNULL(preco, 0) != IFNULL(promoprice, 0) AND
+	 IFNULL(preco, 0) != IFNULL(precoPromoEditor, 0)) AND
+	IFNULL(refprice, 0) != IFNULL(preco, 0) AND :diferenca = 'BASE') OR
        (IFNULL(refprice, 0) != IFNULL(preco, 0) AND :diferenca = 'BASEREF') OR
        (IFNULL(promoprice, 0) != IFNULL(precoPromoEditor, 0) AND
 	(IFNULL(promoprice, 0) != 0 OR IFNULL(precoPromoEditor, 0) != 0) AND
