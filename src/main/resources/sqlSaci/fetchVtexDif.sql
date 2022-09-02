16c61a92e9b09aa4fb8d9e15f72056b99a2e8461
Ivaney Vieira <ivaneyvieira@gmail.com>
2022-09-02 12:15:56 -0300

    Atualização dos filtro dos itens do painel Ctrl+P x Base

--- src/main/resources/sqlSaci/fetchVtexDif.sql
+++ src/main/resources/sqlSaci/fetchVtexDif.sql
@@ -49,9 +49,9 @@ WHERE (skuId LIKE CONCAT(@SKU, '%') OR @SKU = '')
        (IFNULL(precoList, 0) != IFNULL(refprice, 0) AND :diferenca = 'LIST') OR
        (IFNULL(precoCompor, 0) != 0 AND :diferenca = 'COMPOR') OR
        (IFNULL(promoVtex, 0) != 0 AND :diferenca = 'PRICEBASE') OR
-       ((IFNULL(preco, 0) != IFNULL(promoprice, 0) AND
-	 IFNULL(preco, 0) != IFNULL(precoPromoEditor, 0)) AND
-	IFNULL(refprice, 0) != IFNULL(preco, 0) AND :diferenca = 'BASE') OR
+       ((IFNULL(preco, 0) != IFNULL(promoprice, 0) OR
+	 IFNULL(preco, 0) != IFNULL(precoPromoEditor, 0) OR
+	 IFNULL(refprice, 0) != IFNULL(preco, 0)) AND :diferenca = 'BASE') OR
        (IFNULL(refprice, 0) != IFNULL(preco, 0) AND :diferenca = 'BASEREF') OR
        (IFNULL(precoPromoEditor, 0) != IFNULL(preco, 0) AND :diferenca = 'EDITORBASE') OR
        (IFNULL(promoprice, 0) != IFNULL(precoPromoEditor, 0) AND
